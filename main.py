1ddba382f760c38fd5172974efebcd8c3bda665e
SnootyRoom <126325976+SnootyRoom@users.noreply.github.com>
2024-03-19 20:37:07 +0500

    Version 1.0

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('base.html', title='Домашняя страница')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
